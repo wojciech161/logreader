1e53b1816ae7e6fc5e6af3b8433f360b6304807e
Marcin <wojciech161@gmail.com>
2022-01-08 11:20:54 +0100

    project removed

--- src/app/logreader.cpp
+++
@@ -1,8 +0,0 @@
-#include "view/Application.hpp"
-#include "view/ApplicationFactory.hpp"
-
-int main(int argc, char** argv)
-{
-    auto app = view::create_application(argc, argv);
-    return app->run();
-}
--- src/view/src/MainContainer.cpp
+++
@@ -1,9 +0,0 @@
-#include "MainContainer.hpp"
-
-namespace view
-{
-MainContainer::MainContainer()
-{
-    set_border_width(10);
-}
-} // namespace view
